a94a9f6e498f2b8067352716204ebe53bd78c315
FrozstDev <frozstdev@gmail.com>
2023-12-24 15:53:36 -0400

    Dev branch

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 export const welcome = async (req, res) => {
   res.status(200).json({
-    message: 'Welcome to the API Tasacion Application V1'
+    message: 'Welcome to the API Tasacion Application V1 DEV'
   })
 }
