8b634981fcd6b924478dafc61560dc618cd2a542
Carlos <carlos.vargas@assuresoftbolivia.com>
2024-01-02 14:29:53 -0400

    quitando comentarios

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,28 +2,7 @@ import User from '../models/mongo/user.model.js'
 import { handleHttpError } from '../libs/handleHttpMessage/handleHttpError.js'
 import { encryptPassword, comparePassword } from '../libs/authenticationJwt/encrypt.js'
 import { createAccessToken } from '../libs/authenticationJwt/jwt.js'
-// export const signUp = async (req, res) => {
-//   const { username, email, password, roles } = req.body
 
-//   const newUser = new User({
-//     username,
-//     email,
-//     password: await encryptPassword(password)
-//   })
-//   if (roles) {
-//     const foundRoles = await Role.find({ name: { $in: roles } })
-//     newUser.roles = foundRoles.map(role => role._id)
-//   } else {
-//     const role = await Role.findOne({ name: 'user' })
-//     newUser.roles = [role._id]
-//   }
-//   const saveUser = await newUser.save()
-
-//   const token = jwt.sign({ id: saveUser._id }, config.SECRET, {
-//     expiresIn: 86400
-//   })
-//   res.status(200).json({ token })
-// }
 export const signUp = async (req, res) => {
   try {
     const input = req.body
