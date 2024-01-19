4d450f044b2844400c90735c6b861759b6e39f62
FrozstDev <frozstdev@gmail.com>
2024-01-19 10:31:33 -0400

    Endpoints to upload data and delete all articles

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,13 +1,24 @@
 import { Router } from 'express'
-import { getArticles, createArticle, getArticle, updateArticle, deleteArticle } from '../controllers/article.controller.js'
+import {
+  getArticles,
+  createArticle,
+  getArticle,
+  updateArticle,
+  deleteArticle,
+  deleteAllArticles,
+  uploadDataCsv
+} from '../controllers/article.controller.js'
 import { registerArticle } from '../schemas/article.schema.js'
 import { validateSchema } from '../middlewares/validator.middleware.js'
+import { upload } from '../middlewares/multer.js'
 const router = Router()
 
 router.get('/', getArticles)
 router.post('/', validateSchema(registerArticle), createArticle)
+router.post('/import-data', upload.single('csvFile'), uploadDataCsv)
 router.get('/:id', getArticle)
 router.put('/:id', validateSchema(registerArticle.partial()), updateArticle)
+router.delete('/delete-all', deleteAllArticles)
 router.delete('/:id', deleteArticle)
 
 export default router
